de143384ff76d3c701253410ff9a3a4c5224c1f9
MKanunnikau <42643598+matieEKV@users.noreply.github.com>
2025-04-19 20:29:14 +0200

    Add files via upload

---
+++ ShowAndTicket/src/main/java/Show.java
@@ -0,0 +1,17 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matea
+ */
+import java.util.ArrayList;
+public class Show {
+    
+    private String movie;
+    private String time;
+}
